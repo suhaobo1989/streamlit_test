62d7298a166c55bc6ada639cc475e91afb298462
suhaobo1989 <suhaobo1989.com>
2023-03-28 08:51:23 -0500

    added Bigquery connection

--- dependency_check.py
+++ dependency_check.py
@@ -1,5 +1,32 @@
 import pandas as pd
 import time
+import os
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/haobomini/Documents/github/keen-hangar-228019-6e5e2d02d2e7.json"
+
+
+query_job = client.query(
+    """
+    SELECT
+      CONCAT(
+        'https://stackoverflow.com/questions/',
+        CAST(id as STRING)) as url,
+      view_count
+    FROM `bigquery-public-data.stackoverflow.posts_questions`
+    WHERE tags like '%google-bigquery%'
+    ORDER BY view_count DESC
+    LIMIT 10"""
+)
+
+results = query_job.result()  # Waits for job to complete.
+
+for row in results:
+    print("{} : {} views".format(row.url, row.view_count))
+
+data1=results.to_dataframe()
 
 
 def dependency_check():
